f0ad815d2ed93976b0f23ec8af79714136735933
latty <yoyonel@hotmail.com>
2018-09-03 11:07:32 +0200

    Add SSH tunnel (to sql tvty by echelon) command in build.sh

--- build.sh
+++ build.sh
@@ -18,4 +18,22 @@ xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 mkdir -p ./data
 chmod 777 data
 
+# SSH Tunnel
+source data/env.sh
+echo "SSH tunnel (on port=$SSH_PORT, with key=$SSH_KEY_FOR_TUNNELING): $LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT with tunnel host on: $TUNNEL_HOST ..."
+ssh \
+	-o StrictHostKeyChecking=no \
+	-i $SSH_KEY_FOR_TUNNELING \
+	-N $TUNNEL_HOST \
+	-L $LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT \
+	-p $SSH_PORT \
+	&
+echo "SSH tunnel (on port=$SSH_PORT): $LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT with tunnel host on: $TUNNEL_HOST ... done"
+PID_SSH_CMD=$!
+echo "PID of SSH tunnel=$PID_SSH_CMD"
+
 docker-compose up --build
+
+echo "Kill ssh tunnel with pid=$PID_SSH_CMD ..."
+kill -9 $PID_SSH_CMD
+echo "Kill ssh tunnel with pid=$PID_SSH_CMD ... done"
